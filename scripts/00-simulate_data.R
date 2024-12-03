8af8407c7d857dfac55768abf1d1cb138e1c2708
Julia Lee <jlee.lee@mail.utoronto.ca>
2024-12-03 00:41:15 -0500

    Edited data simulation script and saved simulated dataset to "simulated data" folder

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,52 +1,63 @@
 #### Preamble ####
-# Purpose: Simulates a dataset of Australian electoral divisions, including the 
-  #state and party that won each division.
-# Author: Rohan Alexander
-# Date: 26 September 2024
-# Contact: rohan.alexander@utoronto.ca
+# Purpose: Simulate the Neighbourhood Profiles Data from Open Data Toronto
+# Author: Julia Lee
+# Date: 28 November 2024
+# Contact: jlee.lee@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: The `tidyverse` package must be installed
-# Any other information needed? Make sure you are in the `starter_folder` rproj
+# Pre-requisites: Review the plan and sketches for the analysis in the "plan" folder
+# Any other information needed? N/A
 
 
-#### Workspace setup ####
+#### Setting Up the Workspace ####
+
 library(tidyverse)
-set.seed(853)
 
+#### Simulating the Data ####
 
-#### Simulate data ####
-# State names
-states <- c(
-  "New South Wales",
-  "Victoria",
-  "Queensland",
-  "South Australia",
-  "Western Australia",
-  "Tasmania",
-  "Northern Territory",
-  "Australian Capital Territory"
-)
+set.seed(059)
 
-# Political parties
-parties <- c("Labor", "Liberal", "Greens", "National", "Other")
+# Want to create a table that shows the total number of census families, 
+# the number of 2-person, 3-person, 4-person, and 5-person families, the average size
+# of census families, the average number of children in census families, and the average
+# after-tax income of census families for each neighbourhood in Toronto
 
-# Create a dataset by randomly assigning states and parties to divisions
-analysis_data <- tibble(
-  division = paste("Division", 1:151),  # Add "Division" to make it a character
-  state = sample(
-    states,
-    size = 151,
-    replace = TRUE,
-    prob = c(0.25, 0.25, 0.15, 0.1, 0.1, 0.1, 0.025, 0.025) # Rough state population distribution
-  ),
-  party = sample(
-    parties,
-    size = 151,
-    replace = TRUE,
-    prob = c(0.40, 0.40, 0.05, 0.1, 0.05) # Rough party distribution
-  )
-)
+## Define Toronto neighbourhoods - for the purposes of simulation, will only include names
+# and numbers for 20 Toronto neighbourhoods out of 158
 
+neighbourhood_names = c("West Humber-Clairville", "Mount Olive-Silverstone-Jamestown",
+                        "Thistletown-Beaumond Heights", "Rexdale-Kipling", "Elms-Old Rexdale",
+                        "Kingsview Village-The Westway", "Willowridge-Martingrove-Richview",
+                        "Humber Heights-Westmount", "Edenbridge-Humber Valley",
+                        "Princess-Rosethorn", "Eringate-Centennial-West Deane", "Markland Wood",
+                        "Etobicoke West Mall", "Kingsway South", "Stonegate-Queensway", "New Toronto",
+                        "Long Branch", "Alderwood", "Humber Summit", "Humbermede")
 
-#### Save data ####
-write_csv(analysis_data, "data/00-simulated_data/simulated_data.csv")
+neighbourhood_number = c(1:20)
+
+## Creating values for each of the 20 neighbourhoods
+
+num_obs = 20
+
+simulated_data <- 
+  tibble(
+    neighbourhood_name = neighbourhood_names,
+    neighbourhood_number = neighbourhood_number,
+    num_census_fam = rpois(n = num_obs, lambda = 9000), 
+    two_person_fam = rpois(n = num_obs, lambda = 5000),
+    three_person_fam = rpois(n = num_obs, lambda = 3000),
+    four_person_fam = rpois(n = num_obs, lambda = 2000),
+    five_plus_person_fam = rpois(n = num_obs, lambda = 1000),
+    avg_fam_size = sample(seq(from = 2, to = 3, by = 0.1), 
+                         num_obs, replace = TRUE),
+    avg_num_children = sample(seq(from = 1.5, to = 2, by = 0.1), 
+                              num_obs, replace = TRUE),
+    avg_income = rpois(n = num_obs, lambda = 150000))
+
+view(simulated_data)
+
+#### Saving the Data ####
+
+# Save the simulated data as a csv file in data/raw_data
+# The csv file will be called "simulated_data.csv"
+
+write_csv(simulated_data, file = "data/00-simulated_data/simulated_data.csv")
